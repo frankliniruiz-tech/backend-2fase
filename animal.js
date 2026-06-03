77a238ebec1cea03e8466802dd325148ab193a5d
frankliniruiz-tech <franklin.iruiz@escola.pr.gov.br>
2026-06-03 13:53:19 +0000

    versao1

--- animal.js
+++ animal.js
@@ -1,1 +1,11 @@
+const animal = {
+    nome: "Polla Negra", 
+    especie: "Ave",
+    idade: 4, //numeber
+    tutor: false,
+    vacinado: false
 
+};
+
+//console.log(animal);
+console.log(animal.nome);
