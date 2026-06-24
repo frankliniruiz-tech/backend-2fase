2216781b6da8d2bbeecf3843eb3bd06ab29e8291
frankliniruiz-tech <franklin.iruiz@escola.pr.gov.br>
2026-06-24 13:52:32 +0000

    versao1

--- curtidas.js
+++ curtidas.js
@@ -1,2 +1,19 @@
-const feedJSON = require('./feed.json')
+const feedJSON = require('./feed.json');
 
+//Adicionar uma curtida ao pelo id
+function curtir(feed, id){
+    const post = feed[0].id === id ? feed [0];
+}
+
+if (!post) {
+    console.log("Post não encontrado.");
+    return;
+}
+
+if(!post.publicado) {
+    console.log("Não é possível curtir um rascunho.");
+    return;
+}
+
+post.metricas.curtidas++;
+console.log(`Post de @${post.autor} agora tem @${post.metricas.curtidas} curtidas.`);
